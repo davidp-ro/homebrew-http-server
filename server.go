dd40563a7510d89fa9904552b079767280401bf1
David Pescariu <davidpescariu12@gmail.com>
2024-07-10 14:10:06 +0200

    update: example server

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/davidp-ro/homebrew-http-server/http"
 	"github.com/davidp-ro/homebrew-http-server/tcp"
 )
 
@@ -10,12 +11,27 @@ func main() {
 	tcp.Server{
 		Network: tcp.NetworkTCP,
 		Address: ":8080",
-		OnConnection: func(data []byte, err error) {
+		OnConnection: func(data []byte, err error) []byte {
+			// fmt.Printf(">> REQUEST:\n%q\n<< END\n\n", data)
+
 			if err != nil {
 				panic(err)
 			}
 
-			fmt.Printf("Received: %s\n", data)
+			parsed, err := http.ParseRawRequest(data)
+			if err != nil {
+				panic(err)
+			}
+
+			headers := ""
+			for k, v := range parsed.Headers {
+				headers += fmt.Sprintf("%s: %s\n", k, v)
+			}
+
+			fmt.Println(parsed.Body)
+
+			return []byte("HTTP/1.1 200 OK\n\nOK")
+			// return []byte(fmt.Sprintf("HTTP/1.1 200 OK\n\n%s\n%s\n%s", parsed.Path, parsed.QueryParams["param"], headers))
 		},
 	}.Start()
 }
